d5ce7d69ecdec0f31618f1e081eb589f0edcd51c
aponomarev <aponomarev@tradingview.com>
2020-03-09 19:24:09 +0300

    improved utils

--- packages/react-lightweight-charts/src/utils.ts
+++ packages/react-lightweight-charts/src/utils.ts
@@ -1,27 +1,27 @@
 import {CrosshairMode, LineStyle, LineType, PriceLineSource, PriceScaleMode} from 'lightweight-charts';
 
-export const CROSSHAIR_MODE: Record<'NORMAL' | 'MAGNET', CrosshairMode> = {
-	NORMAL: 0,
-	MAGNET: 1,
-}
-export const LINE_STYLE: Record<'SOLID' | 'DOTTED' | 'DASHED' | 'LARGE_DASHED' | 'SPARSE_DOTTED', LineStyle> = {
-	SOLID: 0,
-	DOTTED: 1,
-	DASHED: 2,
-	LARGE_DASHED: 3,
-	SPARSE_DOTTED: 4,
-}
-export const LINE_TYPE: Record<'SIMPLE' | 'WITH_STEPS', LineType> = {
-	SIMPLE: 0,
-	WITH_STEPS: 1,
-}
-export const PRICE_LINE_SOURCE: Record<'LAST_BAR' | 'LAST_VISIBLE', PriceLineSource> = {
-	LAST_BAR: 0,
-	LAST_VISIBLE: 1,
-}
-export const PRICE_SCALE_MODE: Record<'NORMAL' | 'LOGARITHMIC' | 'PERCENTAGE' | 'INDEXED_TO_100', PriceScaleMode> = {
-	NORMAL: 0,
-	LOGARITHMIC: 1,
-	PERCENTAGE: 2,
-	INDEXED_TO_100: 3,
-}
+export const CROSSHAIR_MODE = {
+	NORMAL: CrosshairMode.Normal,
+	MAGNET: CrosshairMode.Magnet,
+} as const;
+export const LINE_STYLE = {
+	SOLID: LineStyle.Solid,
+	DOTTED: LineStyle.Dotted,
+	DASHED: LineStyle.Dashed,
+	LARGE_DASHED: LineStyle.LargeDashed,
+	SPARSE_DOTTED: LineStyle.SparseDotted,
+} as const;
+export const LINE_TYPE = {
+	SIMPLE: LineType.Simple,
+	WITH_STEPS: LineType.WithSteps,
+} as const;
+export const PRICE_LINE_SOURCE = {
+	LAST_BAR: PriceLineSource.LastBar,
+	LAST_VISIBLE: PriceLineSource.LastVisible,
+} as const;
+export const PRICE_SCALE_MODE = {
+	NORMAL: PriceScaleMode.Normal,
+	LOGARITHMIC: PriceScaleMode.Logarithmic,
+	PERCENTAGE: PriceScaleMode.Percentage,
+	INDEXED_TO_100: PriceScaleMode.IndexedTo100,
+} as const;
